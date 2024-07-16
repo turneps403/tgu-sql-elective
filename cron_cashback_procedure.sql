ce9fa31052dcf1b4a24d439a7e16c4ddd5d82387
turneps403 <turneps403@mail.ru>
2024-07-16 11:05:32 +0300

    - indent

--- cron_cashback_procedure.sql
+++ cron_cashback_procedure.sql
@@ -14,18 +14,18 @@ BEGIN
     ctr_id := (SELECT id FROM ContractTypes WHERE ShortCode ='BANKING');
 
     --меняем тип проводки с плановой на фактическую
-	UPDATE Transactions 
+    UPDATE Transactions 
     SET is_plan = false
-	FROM Contracts cts
+    FROM Contracts cts
         JOIN Accounts acc ON acc.InstownerID = cts.InstitutionID AND acc.CurrencyID = ccy_id AND acc.AccType = 1
         JOIN Transactions trs ON trs.receiverid = acc.id 
-	WHERE 
+    WHERE 
         cts.DateEnd IS NULL
         AND cts.ContractTypeID = ctr_id
         AND DATE_PART('month', cts.DateStart) = DATE_PART('month', calcdate)
         AND DATE_PART('day', cts.DateStart) = DATE_PART('day', calc_date)
         AND trs.trandatetime BETWEEN (ARG_CALC_DATE - interval '1 month') AND (CURRENT_DATE - interval '1 sec')
-	    AND trs.is_plan = true
+        AND trs.is_plan = true
         AND trs.transaction_portfolio_id = tp_id;
 END;
 $$;
